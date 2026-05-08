5fbd6b07ebe64cdea544523ab1b328f511135d5d
Chinmayee21d <chinmayee@factwise.io>
2026-05-08 21:24:34 +0530

    Clean up: Removed duplicate Case Studies component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,7 +76,6 @@ export default function Home() {
       <Testimonials />
 
       {/* 10: Case Studies */}
-      <CaseStudies />
       <ModernCaseStudies />
       {/* 10 */}
       <IndustryMarquee />
